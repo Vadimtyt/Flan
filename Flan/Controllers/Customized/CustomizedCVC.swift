c36aaf6020c61fbfdfec36ca58eec8ea4e2692c4
Vadimtyt <vadimtyt@mail.ru>
2021-05-22 22:17:08 +0300

    Добавил PresentationController. Изменил логику и внешний вид появления CustomizedDetailVC. Адаптировал UI под устройства без челки

--- Flan/Controllers/Customized/CustomizedCVC.swift
+++ Flan/Controllers/Customized/CustomizedCVC.swift
@@ -11,7 +11,7 @@ private let reuseIdentifier = "customizedCell"
 
 class CustomizedCVC: UICollectionViewController {
     private let itemsPerRow: CGFloat = 2
-    private let sectionPadding: CGFloat = 20
+    private let sectionPadding: CGFloat = 16
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,13 +22,11 @@ class CustomizedCVC: UICollectionViewController {
     // MARK: UICollectionViewDataSource
 
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
 
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of items
         return Int.random(in: 20...30)
     }
 
@@ -42,44 +40,17 @@ class CustomizedCVC: UICollectionViewController {
     }
     
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        TapticFeedback.shared.tapticFeedback(.light)
+        
         let storyboard = UIStoryboard(name: "CustomizedDetail", bundle: nil)
         
         guard let customizedDetailVC = storyboard.instantiateViewController(withIdentifier: "customizedDetail") as? CustomizedDetailVC else { return }
         customizedDetailVC.cake = Cake(number: indexPath.row + 1)
-        self.present(customizedDetailVC, animated: true)
+        
+        customizedDetailVC.modalPresentationStyle = .custom
+        customizedDetailVC.transitioningDelegate = self
+        self.present(customizedDetailVC, animated: true, completion: nil)
     }
-
-    // MARK: UICollectionViewDelegate
-
-    /*
-    // Uncomment this method to specify if the specified item should be highlighted during tracking
-    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment this method to specify if the specified item should be selected
-    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
-    
-    }
-    */
-    
 }
 
 extension CustomizedCVC: UICollectionViewDelegateFlowLayout {
@@ -98,5 +69,11 @@ extension CustomizedCVC: UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
         return sectionPadding
+    }
+}
+
+extension CustomizedCVC: UIViewControllerTransitioningDelegate {
+    func presentationController(forPresented presented: UIViewController, presenting: UIViewController?, source: UIViewController) -> UIPresentationController? {
+        return PresentationController(presentedViewController: presented, presenting: presenting)
     }
 }
--- Flan/Controllers/Customized/CustomizedDetail/CustomizedDetailVC.swift
+++ Flan/Controllers/Customized/CustomizedDetail/CustomizedDetailVC.swift
@@ -10,12 +10,60 @@ import UIKit
 class CustomizedDetailVC: UIViewController {
     var cake: Cake!
     
+    var hasSetPointOrigin = false
+    var pointOrigin: CGPoint?
+    
     @IBOutlet weak var cakeImage: UIImageView!
     @IBOutlet weak var cakeNumberLabel: UILabel!
+    
     override func viewDidLoad() {
+        super.viewDidLoad()
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(panGestureRecognizerAction))
+        view.addGestureRecognizer(panGesture)
+        
         cakeImage.image = cake.image
         cakeNumberLabel.text = "#\(cake.number)"
     }
     
+    override func viewDidLayoutSubviews() {
+        if !hasSetPointOrigin {
+            hasSetPointOrigin = true
+            pointOrigin = self.view.frame.origin
+        }
+    }
+    @objc func panGestureRecognizerAction(sender: UIPanGestureRecognizer) {
+        let translation = sender.translation(in: view)
+        
+        // Not allowing the user to drag the view upward
+        guard translation.y >= 0 else { return }
+        
+        // setting x as 0 because we don't want users to move the frame side ways!! Only want straight up or down
+        view.frame.origin = CGPoint(x: 0, y: self.pointOrigin!.y + translation.y)
+        
+        if sender.state == .ended {
+            let dragVelocity = sender.velocity(in: view)
+            if dragVelocity.y >= 1300 {
+                self.dismiss(animated: true, completion: nil)
+            } else {
+                // Set back to original position of the view controller
+                UIView.animate(withDuration: 0.3) {
+                    self.view.frame.origin = self.pointOrigin ?? CGPoint(x: 0, y: 400)
+                }
+            }
+        }
+    }
+    
+    @IBAction func shareButtonPressed(_ sender: UIButton) {
+        TapticFeedback.shared.tapticFeedback(.light)
+        
+        guard let image = cake.image else { return }
+        let activityVC = UIActivityViewController(activityItems: [image], applicationActivities: nil)
+        activityVC.excludedActivityTypes = [UIActivity.ActivityType.airDrop,UIActivity.ActivityType.addToReadingList]
+        self.present(activityVC, animated: true, completion: nil)
+    }
+    
+    @IBAction func closeButtonPressed(_ sender: UIButton) {
+        TapticFeedback.shared.tapticFeedback(.light)
+        dismiss(animated: true)
+    }
 }
-
